300aaf205050217a7af580ba71a925586cabc153
christykl <ckl@mit.edu>
2025-06-13 19:51:13 -0400

    separate DatasetExemplars and SyntheticExemplars as utils

--- utils/DatasetExemplars.py
+++ utils/DatasetExemplars.py
@@ -2,9 +2,6 @@ import os
 import numpy as np
 import base64
 from PIL import Image
-from collections import defaultdict
-from typing import Dict, List
-
 
 class DatasetExemplars():
     """
@@ -23,13 +20,12 @@ class DatasetExemplars():
         Number of exemplar images to use for each unit.
     path2save : str
         Path to the directory where the results will be saved.
-    unit_dict : dict
-        Dict containing path to each unit: model_name->layer_name->neuron_num
-        {
-        model_name: {
-                layer_name: neuron_list
-                }
-        }
+    model_name : str
+        Name of the neural network model being dissected.
+    layers : list
+        List of layer names in the model to be dissected.
+    units : list
+        List of unit indices to be analyzed. If None, all units are analyzed.
     im_size : int, optional
         Size to which the images will be resized (default is 224).
 
@@ -40,7 +36,7 @@ class DatasetExemplars():
         to the exemplar images and saving visualizations of activated regions.
     """
 
-    def __init__(self, path2exemplars: str, n_exemplars: int, path2save: str, unit_dict: Dict[str, Dict[str, List[int]]], im_size=224, all_units=False):
+    def __init__(self, path2exemplars, path2save, model_name, layers, units, n_exemplars = 15, im_size=224):
 
         """
         Constructs all the necessary attributes for the DatasetExemplars object.
@@ -53,13 +49,12 @@ class DatasetExemplars():
             Number of exemplar images to use for each unit.
         path2save : str
             Path to the directory where the results will be saved.
-        unit_dict : dict
-            Dict containing path to each unit: model_name->layer_name->neuron_num
-            {
-            model_name: {
-                    layer_name: neuron_list
-                    }
-            }
+        model_name : str
+            Name of the neural network model being dissected.
+        layers : list
+            List of layer names in the model to be dissected.
+        units : list
+            List of unit indices to be analyzed. If None, all units are analyzed.
         im_size : int, optional
             Size to which the images will be resized (default is 224).
         """
@@ -67,23 +62,28 @@ class DatasetExemplars():
         self.path2exemplars = path2exemplars
         self.n_exemplars = n_exemplars
         self.path2save = path2save
-        self.unit_dict = unit_dict
+        self.model_name = model_name
+        self.layers = layers
+        self.units = units
         self.im_size = im_size
-        self.exemplars = defaultdict(lambda : defaultdict(dict))
-        self.activations = defaultdict(lambda : defaultdict(dict))
-        self.thresholds = defaultdict(lambda : defaultdict(dict))
-        for model_name, layers in unit_dict.items():
-            for layer in layers:
-                if not all_units:
-                    exemplars, activations, thresholds = self.net_dissect(model_name, layer, unit_dict[model_name][layer])
-                else:
-                    exemplars, activations, thresholds = self.net_dissect(model_name, layer, None)
-                self.exemplars[model_name][layer] = exemplars
-                self.activations[model_name][layer] = activations
-                self.thresholds[model_name][layer] = thresholds
 
-    # TODO - Extract image masking
-    def net_dissect(self, model_name, layer, units, im_size=224):
+        self.exemplars = {}
+        self.activations = {}
+        self.thresholds = {}
+
+        if isinstance(self.layers, list):
+            for layer in self.layers: 
+                exemplars, activations, thresholds = self.net_dissect(layer)
+                self.exemplars[layer] = exemplars
+                self.activations[layer] = activations
+                self.thresholds[layer] = thresholds
+        else:
+            exemplars, activations, thresholds = self.net_dissect(self.layers)
+            self.exemplars[self.layers] = exemplars
+            self.activations[self.layers] = activations
+            self.thresholds[self.layers] = thresholds
+
+    def net_dissect(self,layer,im_size=224):
 
         """
         Dissects the specified layer of the neural network.
@@ -94,8 +94,6 @@ class DatasetExemplars():
 
         Parameters
         ----------
-        model_name : str
-            The name of the model containing the layer to be dissected
         layer : str
             The name of the layer to be dissected.
         im_size : int, optional
@@ -109,19 +107,19 @@ class DatasetExemplars():
             for the specified layer. The images are Base64 encoded strings.
         """
 
-        exp_path = f'{self.path2exemplars}/{model_name}/imagenet/{layer}'
-        activations = np.loadtxt(f'{exp_path}/activations.csv', delimiter=',') # units * exemplars
-        thresholds = np.loadtxt(f'{exp_path}/thresholds.csv', delimiter=',') # units
+        exp_path = f'{self.path2exemplars}/{self.model_name}/imagenet/{layer}'
+        activations = np.loadtxt(f'{exp_path}/activations.csv', delimiter=',')
+        thresholds = np.loadtxt(f'{exp_path}/thresholds.csv', delimiter=',')
         image_array = np.load(f'{exp_path}/images.npy')
         mask_array = np.load(f'{exp_path}/masks.npy')
         all_images = []
         for unit in range(activations.shape[0]):
             curr_image_list = []
-            if units!=None and not(unit in units):
+            if self.units!=None and not(unit in self.units):
                 all_images.append(curr_image_list)
                 continue
             for exemplar_inx in range(min(activations.shape[1],self.n_exemplars)):
-                save_path = os.path.join(self.path2save,'dataset_exemplars',model_name,layer,str(unit),'netdisect_exemplars')
+                save_path = os.path.join(self.path2save,'dataset_exemplars',self.model_name,layer,str(unit),'netdisect_exemplars')
                 if os.path.exists(os.path.join(save_path,f'{exemplar_inx}.png')):
                     with open(os.path.join(save_path,f'{exemplar_inx}.png'), "rb") as image_file:
                         masked_image = base64.b64encode(image_file.read()).decode('utf-8')
@@ -131,7 +129,7 @@ class DatasetExemplars():
                     curr_image = image_array[unit,exemplar_inx]
                     inside = np.array(curr_mask>0)
                     outside = np.array(curr_mask==0)
-                    masked_image = curr_image * inside + 0.1 * curr_image * outside
+                    masked_image = curr_image * inside + 0.25 * curr_image * outside
                     masked_image =  Image.fromarray(np.transpose(masked_image, (1, 2, 0)).astype(np.uint8))
                     masked_image = masked_image.resize([self.im_size, self.im_size], Image.Resampling.LANCZOS)
                     os.makedirs(save_path,exist_ok=True)
@@ -139,7 +137,6 @@ class DatasetExemplars():
                     with open(os.path.join(save_path,f'{exemplar_inx}.png'), "rb") as image_file:
                         masked_image = base64.b64encode(image_file.read()).decode('utf-8')
                     curr_image_list.append(masked_image)
-                    # curr_image_list.append({"image": masked_image, "resize": im_size})
             all_images.append(curr_image_list)
 
         return all_images,activations[:,:self.n_exemplars],thresholds
