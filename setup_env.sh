f177a20d218509e1d4fe0cd8f2b102edc3796bc1
christykl <ckl@mit.edu>
2025-06-13 20:06:34 -0400

    add instdiff and flux requirements

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
 
-conda create -n maia python=3.8
+conda create -n maia python=3.10
 conda activate maia
 conda install --name maia --file environment.txt
---
+++ utils/setup_instdiff_flux.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/cientgu/InstructDiffusion.git
+pip install -r requirements_instdiff_flux.txt
+cd InstructDiffusion
+bash scripts/download_pretrained_instructdiffusion.sh
